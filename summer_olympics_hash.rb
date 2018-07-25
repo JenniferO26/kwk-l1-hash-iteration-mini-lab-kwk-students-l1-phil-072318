67e17ce09344d5022587db2131a7a0a10f715544
Jennifer Okwara <jokwara03@gmail.com>
2018-07-25 20:21:31 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -34,8 +34,9 @@ def iterate_through_hash
     count += 1
   end
   end
-    puts "The "
+
 iterate_through_hash
+
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
